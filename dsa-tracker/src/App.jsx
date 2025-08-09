eaa64fc8d40199be77e0b3ef4849bd7c7281af95
Krishna Vamsi Bommireddy <157903444+KrishnaVamsi2004@users.noreply.github.com>
2025-08-09 20:10:26 +0530

    Add files via upload

---
+++ dsa-tracker/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>🚀 DSA Tracker</h1>
+      <p>Your daily coding journey starts here!</p>
+    </div>
+  )
+}
